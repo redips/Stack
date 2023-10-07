8847511ae2ae584f24feb1f7ad1398eea1b034e2
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2023-10-07 06:48:09 +0200

    fixup! Add the CompositeEventBlockRenderer

--- src/TwigEvent/config/services.php
+++ src/TwigEvent/config/services.php
@@ -3,6 +3,7 @@
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Sylius\TwigEvent\Registry\EventBlocksRegistry;
+use Sylius\TwigEvent\Renderer\CompositeEventBlockRenderer;
 
 return static function (ContainerConfigurator $configurator): void {
     $services = $configurator->services();
@@ -13,7 +14,7 @@ return static function (ContainerConfigurator $configurator): void {
         ])
     ;
 
-    $services->set('twig_event.renderer.composite_event_block')
+    $services->set('twig_event.renderer.composite_event_block', CompositeEventBlockRenderer::class)
         ->args([
             tagged_iterator('twig_event.event_block_renderer'),
         ])
