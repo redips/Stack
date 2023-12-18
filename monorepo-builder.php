8a63cdf4d432f3a4fe213efd8364db60c0f87ba1
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2023-12-18 12:16:11 +0100

    Remove vimeo/psalm

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -15,7 +15,6 @@ return static function (MBConfig $mbConfig): void {
             'phpstan/phpstan' => '^1.10',
             'symfony/flex' => '^2',
             'symplify/monorepo-builder' => '11.2.*',
-            'vimeo/psalm' => '^5.15',
             'phpstan/phpstan-symfony' => '^1.3'
         ],
         ComposerJsonSection::AUTOLOAD_DEV => [
