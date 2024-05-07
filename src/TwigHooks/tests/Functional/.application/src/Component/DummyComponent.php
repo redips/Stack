5bcb4b899e3b4291214329d6683679f88d1135ff
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-05-07 06:59:41 +0200

    [Twig Hooks] Add tests covering rendering a HookableComponent (a regular one) used directly in a template

--- src/TwigHooks/tests/Functional/.application/src/Component/DummyComponent.php
+++ src/TwigHooks/tests/Functional/.application/src/Component/DummyComponent.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace TestApplication\Sylius\TwigHooks\Component;
 
-use Sylius\TwigHooks\LiveComponent\HookableLiveComponentTrait;
+use Sylius\TwigHooks\Twig\Component\HookableComponentTrait;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
-#[AsLiveComponent('app:dummy_live', template: '_component/dummy.html.twig')]
-final class DummyLiveComponent
+#[AsLiveComponent('app:dummy', template: '_component/dummy.html.twig')]
+final class DummyComponent
 {
     use DefaultActionTrait;
-    use HookableLiveComponentTrait;
+    use HookableComponentTrait;
 }
