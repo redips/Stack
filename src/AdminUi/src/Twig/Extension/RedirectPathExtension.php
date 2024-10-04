216799ed11e82320f39b917a0d80d6ca1fff1ea4
Loïc Frémont <lc.fremont@gmail.com>
2024-10-04 09:47:51 +0200

    Redirect path extension

---
+++ src/AdminUi/src/Twig/Extension/RedirectPathExtension.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\AdminUi\Twig\Extension;
+
+use Sylius\Bundle\GridBundle\Storage\FilterStorageInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class RedirectPathExtension extends AbstractExtension
+{
+    private const NUMBER_OF_ROUTE_PROPERTIES = 3;
+
+    public function __construct(
+        private readonly RouterInterface $router,
+        private readonly ?FilterStorageInterface $filterStorage = null,
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('sylius_generate_redirect_path', [$this, 'generateRedirectPath']),
+        ];
+    }
+
+    public function generateRedirectPath(?string $path): ?string
+    {
+        if (null === $path) {
+            return null;
+        }
+
+        $request = Request::create($path);
+
+        try {
+            $routeInfo = $this->router->match($request->getPathInfo());
+        } catch (\Throwable) {
+            return $path;
+        }
+
+        if ([] !== $request->query->all() || $this->hasAdditionalParameters($routeInfo)) {
+            return $path;
+        }
+
+        $route = $routeInfo['_route'];
+
+        return $this->router->generate($route, $this->filterStorage?->all() ?? []);
+    }
+
+    /**
+     * @param mixed[] $routeInfo
+     */
+    private function hasAdditionalParameters(array $routeInfo): bool
+    {
+        return count($routeInfo) > self::NUMBER_OF_ROUTE_PROPERTIES;
+    }
+}
---
+++ src/AdminUi/tests/Functional/Twig/Extension/RedirectPathExtensionTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\AdminUi\Functional\Twig\Extension;
+
+use Sylius\AdminUi\Twig\Extension\RedirectPathExtension;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+final class RedirectPathExtensionTest extends KernelTestCase
+{
+    private RedirectPathExtension $redirectPathExtension;
+
+    protected function setUp(): void
+    {
+        $container = self::getContainer();
+
+        $session = $container->get('session.factory')->createSession();
+        $request = new Request();
+        $request->setSession($session);
+        $container->get('request_stack')->push($request);
+
+        $this->redirectPathExtension = $container->get('sylius_twig_extra.twig.extension.redirect_path');
+        $container->get('sylius.grid.filter_storage')->set(['criteria' => ['enabled' => true]]);
+    }
+
+    public function testItReturnsRedirectPathWithFiltersFromStorageApplied(): void
+    {
+        // Note, it requires an existing route path.
+        $redirectPath = $this->redirectPathExtension->generateRedirectPath('/books');
+
+        $this->assertSame('/books?criteria%5Benabled%5D=1', $redirectPath);
+    }
+
+    public function testItReturnsGivenPathIfRouteHasSomeMoreConfiguration(): void
+    {
+        // TODO ask internal core team member to reproduce this route with "some more configuration"
+        $redirectPath = $this->redirectPathExtension->generateRedirectPath('/admin/ajax/products/search');
+
+        $this->assertSame('/admin/ajax/products/search', $redirectPath);
+    }
+
+    public function testItReturnsGivenPathIfRouteIsNotMatched(): void
+    {
+        $redirectPath = $this->redirectPathExtension->generateRedirectPath('/invalid-path');
+
+        $this->assertSame('/invalid-path', $redirectPath);
+    }
+
+    public function testItReturnsNullIfThePathIsNullAsWell(): void
+    {
+        $redirectPath = $this->redirectPathExtension->generateRedirectPath(null);
+
+        $this->assertNull($redirectPath);
+    }
+}
