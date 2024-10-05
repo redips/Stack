5832d8ae17f181fe4e7cea7dbe50794d7e291f84
Loïc Frémont <lc.fremont@gmail.com>
2024-10-05 14:31:48 +0200

    [AdminUi] Improve login controller

--- src/AdminUi/src/Symfony/Controller/LoginController.php
+++ src/AdminUi/src/Symfony/Controller/LoginController.php
@@ -15,6 +15,7 @@ namespace Sylius\AdminUi\Symfony\Controller;
 
 use Sylius\AdminUi\Symfony\Form\Type\LoginType;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -29,16 +30,17 @@ final class LoginController
     ) {
     }
 
-    public function __invoke(Request $request): Response
+    /**
+     * @param class-string<FormTypeInterface>|null $formType
+     */
+    public function __invoke(Request $request, ?string $formType = null, ?string $template = null): Response
     {
         $lastError = $this->authenticationUtils->getLastAuthenticationError();
         $lastUsername = $this->authenticationUtils->getLastUsername();
 
-        // TODO use a Twig component instead
-        $formType = LoginType::class;
-        $form = $this->formFactory->createNamed('', $formType);
+        $form = $this->formFactory->createNamed('', $formType ?? LoginType::class);
 
-        return new Response($this->twig->render('@SyliusAdminUi/security/login.html.twig', [
+        return new Response($this->twig->render($template ?? '@SyliusAdminUi/security/login.html.twig', [
             'form' => $form->createView(),
             'last_username' => $lastUsername,
             'last_error' => $lastError,
