374bdeaaafd2f0f6aa4dd6db04db61d7a799ebcb
Francis Hilaire <Prometee@users.noreply.github.com>
2025-04-16 19:14:00 +0200

    Refactor debug renderers

--- src/TwigHooks/src/Hook/Renderer/Debug/HookDebugCommentRenderer.php
+++ src/TwigHooks/src/Hook/Renderer/Debug/HookDebugCommentRenderer.php
@@ -13,9 +13,10 @@ declare(strict_types=1);
 
 namespace Sylius\TwigHooks\Hook\Renderer\Debug;
 
+use Sylius\TwigHooks\Debug\DebugAwareRendererInterface;
 use Sylius\TwigHooks\Hook\Renderer\HookRendererInterface;
 
-final class HookDebugCommentRenderer implements HookRendererInterface
+final class HookDebugCommentRenderer implements HookRendererInterface, DebugAwareRendererInterface
 {
     public function __construct(private readonly HookRendererInterface $innerRenderer)
     {
@@ -24,32 +25,35 @@ final class HookDebugCommentRenderer implements HookRendererInterface
     public function render(array $hookNames, array $hookContext = []): string
     {
         $renderedParts = [];
-        $renderedParts[] = $this->getOpeningDebugComment($hookNames);
+        $renderedParts[] = $this->getDebugComment(
+            $hookNames,
+            $hookContext,
+            '%s BEGIN HOOK | name: "%s" %s',
+        );
         $renderedParts[] = trim($this->innerRenderer->render($hookNames, $hookContext));
-        $renderedParts[] = $this->getClosingDebugComment($hookNames);
+        $renderedParts[] = $this->getDebugComment(
+            $hookNames,
+            $hookContext,
+            '%s  END HOOK  | name: "%s" %s',
+        );
 
         return implode(\PHP_EOL, $renderedParts);
     }
 
     /**
      * @param string|array<string> $hooksNames
+     * @param array<string, mixed> $hookContext
      */
-    private function getOpeningDebugComment(string|array $hooksNames): string
+    private function getDebugComment(string|array $hooksNames, array $hookContext, string $format): string
     {
-        return sprintf(
-            '<!-- BEGIN HOOK | name: "%s" -->',
-            is_string($hooksNames) ? $hooksNames : implode(', ', $hooksNames),
-        );
-    }
+        $commentPrefix = $hookContext[self::CONTEXT_DEBUG_PREFIX] ?? self::DEFAULT_DEBUG_PREFIX;
+        $commentSuffix = $hookContext[self::CONTEXT_DEBUG_SUFFIX] ?? self::DEFAULT_DEBUG_SUFFIX;
 
-    /**
-     * @param string|array<string> $hooksNames
-     */
-    private function getClosingDebugComment(string|array $hooksNames): string
-    {
         return sprintf(
-            '<!--  END HOOK  | name: "%s" -->',
+            $format,
+            $commentPrefix,
             is_string($hooksNames) ? $hooksNames : implode(', ', $hooksNames),
+            $commentSuffix,
         );
     }
 }
